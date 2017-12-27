390a6220472d4c5ec5375f894adfbde21f8ca2dd
eranb <eranb@jfrog.com>
2017-12-27 16:45:26 +0200

    Attempt to fix encoding

--- src/main/java/org/jfrog/example/employees/Main.java
+++ src/main/java/org/jfrog/example/employees/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
     public static final Optional<String> PORT = Optional.ofNullable(System.getenv("PORT"));
     public static final Optional<String> HOSTNAME = Optional.ofNullable(System.getenv("HOSTNAME"));
-
     public static void main(String[] args) throws Exception {
         String contextPath = "/" ;
         String appBase = ".";
